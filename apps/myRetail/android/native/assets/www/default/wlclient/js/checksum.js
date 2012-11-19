1f2ccfd5622e728beff3779beaf3e4b4bba1181a
Michael Zabka <zabka.michael@seznam.cz>
2012-11-19 16:13:24 +0100

    ignore native android

--- apps/myRetail/android/native/assets/www/default/wlclient/js/checksum.js
+++ apps/myRetail/android/native/assets/www/default/wlclient/js/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":2037427390,"date":1353286384478,"machine":"Samuel"};
-/* Date: Mon Nov 19 01:53:04 CET 2012 */
+var WL_CHECKSUM = {"checksum":2037427390,"date":1353337395044,"machine":"Gabriela"};
+/* Date: Mon Nov 19 16:03:15 CET 2012 */
