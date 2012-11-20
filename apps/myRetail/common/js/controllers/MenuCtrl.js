cf9c0b996ba4064944cbb5353b109e5e20db9364
Michael Zabka <zabka.michael@seznam.cz>
2012-11-20 23:31:40 +0100

    menu ctrl

--- apps/myRetail/common/js/controllers/MenuCtrl.js
+++ apps/myRetail/common/js/controllers/MenuCtrl.js
@@ -7,12 +7,15 @@ function MenuCtrl($scope) {
 	$scope.menuItems = [
 		{
 			name: 'Načíst Účet',
+			controller: 'LoadAccountCtrl',
 			url: '/loadAccount'
 		},{
 			name: 'Nabídka',
+			controller: 'OfferListCtrl',
 			url: '/offer-list'
 		},{
 			name: 'QR code',
+			controller: 'ShowQRCodeCtrl',
 			url: '/show-qr-code'
 		}
 	];
