eaa7c79d5294fe86b8bdba5f9b493f8d253afa48
Michael Zabka <zabka.michael@seznam.cz>
2013-01-21 19:53:10 +0100

    server refactor

--- server/app/app.js
+++ server/app/app.js
@@ -1,24 +1,63 @@
 
+var mongoose = require('mongoose');
 var express = require('express');
 
-var router = require('./router');
-var server = require('./server');
+var config = require('./config/config');
 
 var l = require('./services/logDispatcher');
+var router = require('./router');
 
 var app = express();
 
-router.route(app);
 
-var db = server.dbConnect();
+var startServer = function () {
+
+	app.listen(config.server.port).on('error', function (e) {
+		l.error(e);
+	});
+	l.info('Server is running on port '+config.server.port);
+};
+
+var stopServer = function () {
+
+};
+
+var dbReconnect = function () {
+	l.info('Server is restarting');
+	var db = dbConnect();
+};
+
+var dbConnect = function () {
+	mongoose.connect(config.db.url);
+	return mongoose.connection;
+};
+
+var delayedDbReconnect = function () {
+	setTimeout(dbReconnect, 2000);
+};
+
+
+
+
+// Start app
+router.route(app);
+var db = dbConnect();
+
+
+
+// Events
+mongoose.connection.on('error', function (e) {
+	l.error('Failed to reconnect database');
+	delayedDbReconnect();
+});
 
 db.on('open', function () {
 	l.info('Database connection opened');
-	server.start(app);
+	startServer();
 });
 
 db.on('close', function () {
 	l.error('Database connection closed');
-	server.stop(app);
-	server.dbReconnect();
+	stopServer();
+	dbReconnect();
 });
