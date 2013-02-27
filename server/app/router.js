5b4d9eecf4842eb7d03187a28f56be58f794ccce
Michael Zabka <zabka.michael@seznam.cz>
2013-02-27 15:26:36 +0100

    commented router

--- server/app/router.js
+++ server/app/router.js
@@ -13,11 +13,15 @@ var PurchaseCtrl = require('./controllers/PurchaseCtrl');
 exports.route = function (app) {
 	
 	app.configure(function () {
+		// Allow cross domain requests by AJAX (server is on other domain)
 		app.use(allowCrossDomain);
+		// set simple routing from express
 		app.use(app.router);
+		// set static routing on mobile application frontend
 		app.use(express.static(config.path.frontendBasePath));
 	});
 
+	// simple routing on controller and action (like MVC)
 	app.all('/offers', OfferCtrl.offers);
 	app.all('/purchases', PurchaseCtrl.purchases);
 };
